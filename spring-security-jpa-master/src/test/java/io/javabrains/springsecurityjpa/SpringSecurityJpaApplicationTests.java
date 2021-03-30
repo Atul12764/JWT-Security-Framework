69b9f8c060aa8153721d2795ae857733a6cbca6c
Atul12764 <42302907+Atul12764@users.noreply.github.com>
2021-03-31 00:40:14 +0530

    Add files via upload

---
+++ spring-security-jpa-master/src/test/java/io/javabrains/springsecurityjpa/SpringSecurityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.springsecurityjpa;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class SpringSecurityJpaApplicationTests {
+
+		public void contextLoads() {
+	}
+
+}
